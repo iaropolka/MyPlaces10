0841f33e8c6ee8393c0a604c8f3c9ac8cfde83fc
Iaropolka <savenkovsergey76@yandex.ru>
2020-11-13 11:36:23 +0300

    Initial Commit

---
+++ MyPlaces10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces10
+//
+//  Created by Сергей Савенков on 13.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
