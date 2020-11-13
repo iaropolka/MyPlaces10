f05fa45c4b8849ac398b4e8a0e1d4415adb9f9da
Iaropolka <savenkovsergey76@yandex.ru>
2020-11-14 02:42:06 +0300

    Custom Table View Cell

---
+++ MyPlaces10/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces10
+//
+//  Created by Сергей Савенков on 13.11.2020.
+//
+
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+
+}
